64e7eac0a38ab2e82385d977611cbbc634d5cc5e
Estere <es.mancevica@gmail.com>
2025-12-01 19:33:30 +0200

    added redeeming check - only authorized redeemers allowed

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,6 +1,7 @@
 from wake.testing import *
 from pytypes.contracts.Coupons import Coupon1155
 
+
 # To launch a new development chain
 @chain.connect()
 # To connect to an existing chain
@@ -12,34 +13,58 @@ def test_coupon():
     owner = chain.accounts[0]
     issuer = chain.accounts[1]
     other = chain.accounts[2]
+    allowed_redeemer_1 = chain.accounts[3]
+    allowed_redeemer_2 = chain.accounts[4]
 
     coupon.addIssuer(issuer, from_=owner)
     # Only owner can grant issuer rights
-    with must_revert():
-       coupon.addIssuer(other, from_=issuer)
+    with must_revert():  # TODO: theese things "swallow" error messages, at least to my experience
+        coupon.addIssuer(other, from_=issuer)
 
     coupon.mint(issuer, 10, from_=issuer)
     # Only issuer can mint coupons
     with must_revert():
         coupon.mint(other, 10, from_=other)
-    
+
     # Check balance
     print("Issuer balance:", coupon.balanceOf(issuer, 1))
     print("Other balance:", coupon.balanceOf(other, 1))
 
-    print("Transfer 5 coupons from issuer to other")
+    print("Transfer 1: 5 coupons from issuer to other")
     coupon.safeTransferFrom(
-        from__=issuer,
-        to_=other,
-        id=1,
-        value_=5,
-        data=b"",
-        from_=issuer
+        from__=issuer, to_=other, id=1, value_=5, data=b"", from_=issuer
     )
-    print("Issuer balance after transfer:", coupon.balanceOf(issuer, 1))
-    print("Other balance after transfer:", coupon.balanceOf(other, 1))
+    print("  Issuer balance after transfer 1:", coupon.balanceOf(issuer, 1))
+    print("  Other balance after transfer 1:", coupon.balanceOf(other, 1))
 
+    print("Transfer 2: 3 coupons from issuer to allowed_redeemer_1")
+    coupon.safeTransferFrom(
+        from__=issuer, to_=allowed_redeemer_1, id=1, value_=3, data=b"", from_=issuer
+    )
+    print("  Issuer balance after transfer 2:", coupon.balanceOf(issuer, 1))
+    print(
+        "  Allowed_redeemer_1 balance after transfer 2:",
+        coupon.balanceOf(allowed_redeemer_1, 1),
+    )
+
+    coupon.addRedeemers([allowed_redeemer_1, allowed_redeemer_2], from_=owner)
+
+    print("Redeem 2 coupons")
     # Redeem 2 coupons
-    coupon.redeem(1, 2, from_=other)
-    print("Other balance after redeem:", coupon.balanceOf(other, 1))
+    try:
+        coupon.redeem(couponID=1, amount=2, from_=other)
+        print("  other redemption worked")
+    except Exception as e:
+        print("  redeem(other) reverted: ", e)
 
+    try:
+        coupon.redeem(couponID=1, amount=2, from_=allowed_redeemer_1)
+        print("  allowed_redeemer_1 redemption worked")
+    except Exception as e:
+        print("  redeem(allowed_redeemer_1) reverted: ", e)
+
+    print("Other balance after redeem:", coupon.balanceOf(other, 1))
+    print(
+        "allowed_redeemer_1 balance after redeem:",
+        coupon.balanceOf(allowed_redeemer_1, 1),
+    )
