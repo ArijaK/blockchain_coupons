f03e32ba3f5b637037d6713f57c186160a7f6bbf
ArijaK <a.kalnina2002@inbox.lv>
2025-12-23 15:13:33 +0200

    Let's keep 2 spaces for indent

--- blockchain/contracts/Coupons.sol
+++ blockchain/contracts/Coupons.sol
@@ -6,76 +6,76 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 // msg.sender for Ownable makes the deployer the owner
 contract Coupon1155 is ERC1155, Ownable(msg.sender) {
-    // Voucher type (maximum 2^256 types possible)
-    // NOTE: Let's hope we wont have more than (1x10^77) coupon types created
-    uint256 public couponIDCounter;
+  // Voucher type (maximum 2^256 types possible)
+  // NOTE: Let's hope we wont have more than (1x10^77) coupon types created
+  uint256 public couponIDCounter;
 
-    // issuer and redeemer lists - for faster look up
-    mapping(address => bool) public isAllowedRedeemer;
-    mapping(address => bool) public isIssuer;
+  // issuer and redeemer lists - for faster look up
+  mapping(address => bool) public isAllowedRedeemer;
+  mapping(address => bool) public isIssuer;
 
-    // In "https://" we can write where metadata come from (URI)
-    constructor() ERC1155("") {
-        // Contract deployer is automatically an issuer
-        isIssuer[msg.sender] = true;
+  // In "https://" we can write where metadata come from (URI)
+  constructor() ERC1155("") {
+    // Contract deployer is automatically an issuer
+    isIssuer[msg.sender] = true;
+  }
+
+  // Grant issuer rights (only owner can assign)
+  function addIssuer(address account) external onlyOwner {
+    isIssuer[account] = true;
+  }
+
+  // Create redeemers list
+  function addRedeemers(address[] memory redeemers) public {
+    for (uint256 i = 0; i < redeemers.length; i++) {
+      isAllowedRedeemer[redeemers[i]] = true;
     }
+  }
 
-    // Grant issuer rights (only owner can assign)
-    function addIssuer(address account) external onlyOwner {
-        isIssuer[account] = true;
-    }
+  // Issuers can create (mint) new coupons
+  // TODO: Add expiry date (uint256 expiryTimestamp)
+  // require(expiryTimestamp > block.timestamp, "Expiry must be in future");
+  // couponExpiry[couponId] = expiryTimestamp;
+  // Lookup table: mapping(uint256 => uint256) public couponExpiry;
+  // Redemption: require(block.timestamp <= couponExpiry[couponId], "Coupon expired");
+  function mint(address to, uint256 amount) external {
+    require(
+      isIssuer[msg.sender],
+      "Not authorized to create (mint) coupons"
+    );
 
-    // Create redeemers list
-    function addRedeemers(address[] memory redeemers) public {
-        for (uint256 i = 0; i < redeemers.length; i++) {
-            isAllowedRedeemer[redeemers[i]] = true;
-        }
-    }
+    uint256 couponID = ++couponIDCounter;
+    // In "" metadata can be added (see constructor)
+    _mint(to, couponID, amount, "");
+  }
 
-    // Issuers can create (mint) new coupons
-    // TODO: Add expiry date (uint256 expiryTimestamp)
-    // require(expiryTimestamp > block.timestamp, "Expiry must be in future");
-    // couponExpiry[couponId] = expiryTimestamp;
-    // Lookup table: mapping(uint256 => uint256) public couponExpiry;
-    // Redemption: require(block.timestamp <= couponExpiry[couponId], "Coupon expired");
-    function mint(address to, uint256 amount) external {
-        require(
-            isIssuer[msg.sender],
-            "Not authorized to create (mint) coupons"
-        );
+  // Everyone can distribute coupons (without QR code option)
+  // NOTE: If you want to restrict distribution
+  // function distribute(
+  //     address from, address to,
+  //     uint256 couponID, uint256 amount
+  // ) external {
+  //     require(isIssuer[msg.sender], "Not authorized to distribute");
+  //     safeTransferFrom(from, to, couponID, amount, "");
+  // }
 
-        uint256 couponID = ++couponIDCounter;
-        // In "" metadata can be added (see constructor)
-        _mint(to, couponID, amount, "");
-    }
+  // NOTE: We can add burning when expiry date is reached
 
-    // Everyone can distribute coupons (without QR code option)
-    // NOTE: If you want to restrict distribution
-    // function distribute(
-    //     address from, address to,
-    //     uint256 couponID, uint256 amount
-    // ) external {
-    //     require(isIssuer[msg.sender], "Not authorized to distribute");
-    //     safeTransferFrom(from, to, couponID, amount, "");
-    // }
+  // Redemption (burning)
+  function redeem(uint256 couponID) external {
+    // NOTE: Removes from circulation, does not destroy ID
+    // If there were 100 such coupons and 1 was burned,
+    // only 99 will remain in circulation (1 will be unusable anymore)
+    // If there was 1 such coupon and 1 was burned,
+    // no more will remain, but the ID will still exist
 
-    // NOTE: We can add burning when expiry date is reached
+    // only authorized redeemers can redeem
+    require(
+      isAllowedRedeemer[msg.sender],
+      "Not authorized to redeem coupon."
+    );
+    require(balanceOf(msg.sender, couponID) > 0, "No tokens to redeem");
 
-    // Redemption (burning)
-    function redeem(uint256 couponID) external {
-        // NOTE: Removes from circulation, does not destroy ID
-        // If there were 100 such coupons and 1 was burned,
-        // only 99 will remain in circulation (1 will be unusable anymore)
-        // If there was 1 such coupon and 1 was burned,
-        // no more will remain, but the ID will still exist
-
-        // only authorized redeemers can redeem
-        require(
-            isAllowedRedeemer[msg.sender],
-            "Not authorized to redeem coupon."
-        );
-        require(balanceOf(msg.sender, couponID) > 0, "No tokens to redeem");
-
-        _burn(msg.sender, couponID, 1);
-    }
+    _burn(msg.sender, couponID, 1);
+  }
 }
