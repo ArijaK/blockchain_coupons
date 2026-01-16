6cb7d213cdf01e3e5063de11a87e891ba7d053ea
Estere <es.mancevica@gmail.com>
2026-01-16 18:19:23 +0200

    quick host

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,8 @@ server.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
 
-server.listen({ port: 8000 }, (err, address) => {
+
+server.listen({ port: 8000, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
