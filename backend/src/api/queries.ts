55964267397c863803a0c7b5969f87c622a1280c
ArijaK <a.kalnina2002@inbox.lv>
2026-01-17 15:21:52 +0200

    Make it async

--- backend/src/api/queries.ts
+++ backend/src/api/queries.ts
@@ -3,24 +3,26 @@ import { db } from "../services/database.js";
 
 // Coupon instances and types
 export const couponsQueries = {
-  findByID(id: bigint) {
+  async findByID(id: bigint) {
     return db.query(
       "SELECT * FROM coupons WHERE coupon_id = $1",
       [id]
     ).then(r => r.rows[0]);
   },
   
-  findByOwner(address: string) {
-    return db.query(
+  async findByOwner(address: string) {
+    const r = await db.query(
       "SELECT * FROM coupons WHERE owner_id = $1",
       [address]
-    ).then(r => r.rows);
+    );
+    return r.rows;
   },
 
-  findByIssuer(address: string) {
-    return db.query(
+  async findByIssuer(address: string) {
+    const r = await db.query(
       "SELECT * FROM coupon_types WHERE issuer_id = $1",
       [address]
-    ).then(r => r.rows);
+    );
+    return r.rows;
   }
 }
